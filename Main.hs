99b79e3ff21e9ac8b31b56b10aebb12ae80999c9
James Deery <james.deery@jadex.org>
2015-01-25 22:26:11 +0000

    Add stations and lines in Main for debugging

--- Main.hs
+++ Main.hs
@@ -9,6 +9,16 @@ getTube = readTracksFile "tube.tracks"
 getTubeServices :: IO Services
 getTubeServices = readServicesFile "tube.services"
 
+earlsCourt, euston :: Station
+earlsCourt = Station "Earl's Court"
+euston = Station "Euston"
+
+district, piccadilly, victoria, northern :: Line
+district = Line "District Line"
+piccadilly = Line "Piccadilly Line"
+victoria = Line "Victoria Line"
+northern = Line "Northern Line"
+
 main :: IO ()
 main = do
         tube <- getTube
