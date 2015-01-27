6f0c80dded584fef3f9e3849b3ff9c6a176e92a1
James Deery <james.deery@jadex.org>
2015-01-27 19:13:16 +0000

    Run a single train system

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Tracks.Network
 import Tracks.Service
 import Tracks.Train
+import Control.Concurrent (threadDelay)
 
 foxhole, riverford, tunwall, maccton, welbridge :: Station
 foxhole = Station "Foxhole"
@@ -44,4 +45,31 @@ main = do
         print network
         print foxLine
         print newLine
+        let service = getLineService services foxLine 1
+        print $ isServiceValid network foxLine service
+        case startService services signals foxLine 1 of
+            Just result -> do
+                runTrain result
+                return ()
+            Nothing     -> print ":("
 
+main2 :: IO ()
+main2 = do
+        network <- readTracksFile "tube.tracks"
+        services <- readServicesFile "tube.services"
+        let victoria = Line "Victoria Line"
+            service = getLineService services victoria 1
+            signals = clear network
+        print $ isServiceValid network victoria service
+        case startService services signals victoria 1 of
+            Just result -> do
+                runTrain result
+                return ()
+            Nothing     -> print ":("
+
+runTrain :: (Signals, Train) -> IO (Signals, Train)
+runTrain (signals, train) = do
+        print train
+        let (signals', train') = stepTrain signals train
+        threadDelay $ 300 * 1000
+        runTrain (signals', train')
